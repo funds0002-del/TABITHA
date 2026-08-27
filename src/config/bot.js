db1c5ae7471e6ad6859db992f18206cbe28db451
funds0002-del <funds.0002@gmail.com>
2026-08-27 16:18:56 +0000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "watching over 3DCEASED",     // this is what people actually see
+        state: "WATCHING OVER 3DCEASED",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
