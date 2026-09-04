a7017dfdff69f49cf5d08cc6eda678848326c896
funds0002-del <funds.0002@gmail.com>
2026-09-04 04:50:31 +0000

    Update fight.js

--- src/commands/Fun/fight.js
+++ src/commands/Fun/fight.js
@@ -19,6 +19,8 @@ const STARTING_HP = 100;
 // Beta Testers role
 const BETA_TESTER_ROLE_ID = '1545106467928154173';
 
+const SHOW_TAVERN_IMAGE = false;
+
 // Tabitha's Tavern image
 const TAVERN_IMAGE_URL =
   'https://raw.githubusercontent.com/funds0002-del/TABITHA/main/asset/Tabithas_Tavern_PNG.png';
